894daffa7cfb52ec66f64b908ee23f5f1dabf8db
Shiv Maheshwari <shiv.maheshwari@intimetec.com>
2024-01-15 21:37:14 +0530

    refactored code

--- kaprekar.js
+++ kaprekar.js
@@ -1,67 +1,72 @@
-function hasSimilardigits(digits) {
-    let count = 0;
-    for (let i = 0; i < digits.length - 1; i++) {
-      if (digits[i] === digits[i + 1]) {
-        count++;
-      }
-    }
-    if (digits.length - count == 1) {
-      return true;
-    } else {
-      return false;
+function hasSimilarDigits(digits) {
+  const lengthOfNumber = digits.length
+  let count = 0;
+  for (let i = 0; i < lengthOfNumber - 1; i++) {
+    if (digits[i] === digits[i + 1]) {
+      count++;
     }
   }
-  
-  function arrangeInAscendingOrder(digits){
-    return digits
-    .sort((a, b) => {
-      return a - b;
-    })
-    .join("");
-  }
-  
-  function arrangeInDescendingOrder(digits){
-    return digits
-    .sort((a, b) => {
-      return b - a;
-    })
-    .join("");
-  }
-  
-  function applyKaprekar(digits) {
-    let nextNumber = NaN;
-    let iterations = 0;
-    while (iterations < 8) {
-      iterations = iterations + 1;
-      while (digits.length < 4) {
-        digits.push("0");
-      }
-  
-      let smallestNumber = arrangeInAscendingOrder(digits)
-      let largestNumber = arrangeInDescendingOrder(digits)
-      
-      nextNumber = largestNumber - smallestNumber;
-      digits = nextNumber.toString().split("");
-      if (nextNumber === 6174) {
-        return iterations;
-      }
+  if (lengthOfNumber - count == 1) {
+    return true;
+  } 
+  return false
+}
+
+function arrangeInAscendingOrder(digits){
+  return digits
+  .sort((a, b) => {
+    return a - b;
+  })
+  .join("");
+}
+
+function arrangeInDescendingOrder(digits){
+  return digits
+  .sort((a, b) => {
+    return b - a;
+  })
+  .join("");
+}
+
+function applyKaprekar(digits) {
+  let iterations = 0;
+  while (iterations < 8) {
+    iterations = iterations + 1;
+    while (digits.length < 4) {
+      digits.push("0");
+    }
+
+    const smallestNumber = arrangeInAscendingOrder(digits)
+    const largestNumber = arrangeInDescendingOrder(digits)
+    
+    const nextNumber = largestNumber - smallestNumber;
+    digits = nextNumber.toString().split("");
+    if (nextNumber === 6174) {
+      return iterations;
     }
   }
-  
-  function kaprekarIteration(inputNumber) {
-    let digits = inputNumber.toString().split("");
-    if (digits.length != 4) {
-      console.log("Please enter a valid four-digit number");
-      return;
-    }
-  
-    if (hasSimilardigits(digits)) {
-      console.log(
-        "Please input a different number with atleast two different digits"
-      );
-    } else {
-      console.log("No of iterations", applyKaprekar(digits));
-    }
+}
+
+function validateInputNumber(inputNumberDigits){
+  if (inputNumberDigits.length != 4) {
+    console.log("Please enter a valid four-digit number");
+    return false;
   }
-  
-  kaprekarIteration("5214");
+
+  if (hasSimilarDigits(inputNumberDigits)) {
+    console.log(
+      "Please input a different number with atleast two different digits"
+    );
+    return false
+  }
+
+  return true
+}
+function kaprekarIteration(inputNumber) {
+  let digits = inputNumber.toString().split("");
+  if(validateInputNumber(digits)){
+    console.log("No of iterations", applyKaprekar(digits));
+  }
+}
+
+kaprekarIteration("9831");
